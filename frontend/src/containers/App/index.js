55b879340b04198afcc1e2cf601f21dbf5a26764
alsenda <alvaro@alsenda.com>
2018-12-06 23:39:13 +0100

    first commit

---
+++ frontend/src/containers/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+import Project from '../../components/Project';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Project />
+      </div>
+    );
+  }
+}
+
+export default App;
